bfe735e8bb78926a4ac6d5f8ae90281075d43f4a
golamrabbani93 <golamrabbani9373@gmail.com>
2022-10-25 15:09:38 +0600

    fecth categories data in courses.js file

--- src/pages/Courses/Courses.js
+++ src/pages/Courses/Courses.js
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 const Courses = () => {
+	const [categories, setCategories] = useState([]);
+	console.log('🚀🚀: Courses -> categories', categories);
+	useEffect(() => {
+		fetch('http://localhost:5000/course-category')
+			.then((res) => res.json())
+			.then((data) => setCategories(data));
+	}, []);
 	return (
 		<div>
 			<h2>this is Courses page</h2>
