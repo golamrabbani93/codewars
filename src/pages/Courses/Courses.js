529ae3f6df45d24870d37b02cbbe24dac7f3167e
golamrabbani93 <golamrabbani9373@gmail.com>
2022-10-25 17:59:52 +0600

    show all courses to course page

--- src/pages/Courses/Courses.js
+++ src/pages/Courses/Courses.js
@@ -1,25 +1,30 @@
 import React, {useEffect, useState} from 'react';
-import {Link, useLoaderData} from 'react-router-dom';
+import {Link} from 'react-router-dom';
+import AllCoures from '../shared/CousesSummary/AllCourses/AllCoures';
 import './Courses.css';
 const Courses = () => {
 	const [categories, setCategories] = useState([]);
 
 	useEffect(() => {
-		fetch('http://localhost:5000/course-category')
+		fetch('http://localhost:5000/courses')
 			.then((res) => res.json())
 			.then((data) => setCategories(data));
 	}, []);
 	return (
 		<div className="courses-container grid">
-			<div className="bg-base-100"></div>
-			<div className=" mt-10">
-				<ul className="menu bg-base-300 w-56 rounded-box">
+			<div className="bg-base-100 grid grid-cols-2 gap-6 container ml-5 mt-5">
+				{categories.map((course) => (
+					<AllCoures course={course}></AllCoures>
+				))}
+			</div>
+			<div className="mt-5">
+				<ul className="menu bg-base-300  rounded-box sticky top-0">
 					<li className="menu-title text-2xl">
 						<span>Category</span>
 					</li>
 					{categories.map((cat) => (
-						<li className="hover-bordered" key={cat.id}>
-							<Link to>{cat.name}</Link>
+						<li className="hover-bordered text-xs" key={cat.id}>
+							<Link to>{cat.title}</Link>
 						</li>
 					))}
 				</ul>
