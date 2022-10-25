f9d6eb2498b1c6343e79a8d5e0aeba0d4398c7ce
golamrabbani93 <golamrabbani9373@gmail.com>
2022-10-25 13:07:48 +0600

    add tooltip to hover profile image

--- src/pages/shared/Header/Header.js
+++ src/pages/shared/Header/Header.js
@@ -80,30 +80,7 @@ const Header = () => {
 						CodeWars
 					</Link>
 				</div>
-				<div className="dropdown dropdown-end block lg:hidden">
-					<label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-						<div className="w-10 rounded-full">
-							<img src="https://placeimg.com/80/80/people" alt="" />
-						</div>
-					</label>
-					<ul
-						tabIndex={0}
-						className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
-					>
-						<li>
-							<Link className="justify-between">
-								Profile
-								<span className="badge">New</span>
-							</Link>
-						</li>
-						<li>
-							<Link>Settings</Link>
-						</li>
-						<li>
-							<Link>Logout</Link>
-						</li>
-					</ul>
-				</div>
+
 				<div className="navbar-center hidden lg:flex text-white">
 					<ul className="menu menu-horizontal p-0">
 						<li className="mr-2">
@@ -133,8 +110,7 @@ const Header = () => {
 					</select>
 					<Link className="btn">Get started</Link>
 				</div>
-
-				<div className="dropdown dropdown-end hidden lg:block">
+				<div className="dropdown dropdown-end " title="Golam Rabbani">
 					<label tabIndex={0} className="btn btn-ghost btn-circle avatar">
 						<div className="w-10 rounded-full">
 							<img src="https://placeimg.com/80/80/people" alt="" />
