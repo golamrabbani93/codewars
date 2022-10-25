f54a1a2a64be53b204d30aeaad1e5e9e9a739307
golamrabbani93 <golamrabbani9373@gmail.com>
2022-10-25 21:12:46 +0600

    header navlink style change

--- src/pages/shared/Header/Header.js
+++ src/pages/shared/Header/Header.js
@@ -31,7 +31,7 @@ const Header = () => {
 							</label>
 							<ul
 								tabIndex={0}
-								className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 "
+								className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-300 rounded-box w-52 "
 							>
 								<li className="mr-2">
 									<NavLink to="/" end>
