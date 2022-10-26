d650081113de1ebb239081424ff89361274280b1
golamrabbani93 <golamrabbani9373@gmail.com>
2022-10-26 16:24:54 +0600

    add react loader and redirected to the checkout page

--- src/Routes/PrivateRoute/PrivateRoute.js
+++ src/Routes/PrivateRoute/PrivateRoute.js
@@ -1,11 +1,18 @@
 import React, {useContext} from 'react';
 import {Navigate, useLocation} from 'react-router-dom';
 import {AuthContext} from '../../contexts/ContextProvider/ContextProvider';
-
+import RiseLoader from 'react-spinners/RiseLoader';
 const PrivateRoute = ({children}) => {
 	//*get user
-	const {user} = useContext(AuthContext);
+	const {user, loader} = useContext(AuthContext);
 	const location = useLocation();
+	if (loader) {
+		return (
+			<div className="flex justify-center items-center h-96">
+				<RiseLoader color="#F26651"></RiseLoader>
+			</div>
+		);
+	}
 	if (!user) {
 		return <Navigate to="/login" state={{from: location}} replace />;
 	}
