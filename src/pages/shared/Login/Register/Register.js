f7db7bb113e0bbdf318202d41af181dd33dd9b29
golamrabbani93 <golamrabbani9373@gmail.com>
2022-10-26 11:25:27 +0600

    register form complete

--- src/pages/shared/Login/Register/Register.js
+++ src/pages/shared/Login/Register/Register.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-
+import './Register.css';
+import googleimg from '../../../../assets/images/google.png';
+import githubimg from '../../../../assets/images/github.png';
 const Register = () => {
 	return (
 		<div>
 			<div className="hero min-h-screen bg-base-100">
 				<div className="hero-content flex-col lg:flex-row-reverse">
-					<div className="card flex-shrink-0 w-96 max-w-sm shadow-2xl bg-base-300">
+					<form className="card flex-shrink-0 sm:w-96 max-w-sm shadow-2xl bg-base-300">
 						<div className="card-body">
+							<div className="form-control">
+								<label className="label">
+									<span className="label-text">Full Name</span>
+								</label>
+								<input type="text" placeholder="name" className="input input-bordered" />
+							</div>
+							<div className="form-control">
+								<label className="label">
+									<span className="label-text">Photo Url</span>
+								</label>
+								<input type="text" placeholder="photo url" className="input input-bordered" />
+							</div>
 							<div className="form-control">
 								<label className="label">
 									<span className="label-text">Email</span>
@@ -20,19 +34,46 @@ const Register = () => {
 								</label>
 								<input type="text" placeholder="password" className="input input-bordered" />
 								<label className="label">
-									<Link href="#" className="label-text-alt link link-hover">
+									<Link href="#" className="label-text-alt text-warning link link-hover">
 										Forgot password?
 									</Link>
-									<Link href="#" className="label-text-alt link link-hover">
-										Forgot password?
-									</Link>
+									<span className="text-xs">
+										Already have an account
+										<Link to="/login" className="text-warning  link link-hover ml-1">
+											Login
+										</Link>
+									</span>
 								</label>
 							</div>
 							<div className="form-control mt-6">
-								<button className="btn btn-primary">Login</button>
+								<button className="btn btn-primary">Register</button>
+							</div>
+							<span className="border my-2"></span>
+							<div className="social">
+								<span className="text-xl">Continue with </span>
+								<p className="flex justify-center items-center mt-3">
+									<button className="ml-3">
+										<img
+											src={googleimg}
+											alt=""
+											width="40"
+											height="40"
+											style={{borderRadius: '50%'}}
+										/>
+									</button>
+									<button className="ml-3">
+										<img
+											src={githubimg}
+											alt=""
+											width="41"
+											height="40"
+											style={{borderRadius: '50%'}}
+										/>
+									</button>
+								</p>
 							</div>
 						</div>
-					</div>
+					</form>
 				</div>
 			</div>
 		</div>
