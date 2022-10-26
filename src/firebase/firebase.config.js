4ed35933fd4848a63dc6723326e36fc425a81ba8
golamrabbani93 <golamrabbani9373@gmail.com>
2022-10-26 18:31:09 +0600

    add environment-variables

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import {initializeApp} from 'firebase/app';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-	apiKey: 'AIzaSyC_pXpWABdM7djpu7NCwRXekb7GAp-2Sh4',
-	authDomain: 'codewars-hunt.firebaseapp.com',
-	projectId: 'codewars-hunt',
-	storageBucket: 'codewars-hunt.appspot.com',
-	messagingSenderId: '520230621360',
-	appId: '1:520230621360:web:650d05ad87d40814e1ab75',
+	apiKey: process.env.REACT_APP_apiKey,
+	authDomain: process.env.REACT_APP_authDomain,
+	projectId: process.env.REACT_APP_projectId,
+	storageBucket: process.env.REACT_APP_storageBucket,
+	messagingSenderId: process.env.REACT_APP_messagingSenderId,
+	appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
