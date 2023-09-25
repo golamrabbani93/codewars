523050260b149bd7a01d1e40dc936c7cb7192a57
golamrabbani93 <golamrabbani9373@gmail.com>
2023-09-25 16:58:07 +0600

    change theme color setting

--- src/pages/shared/Header/Header.js
+++ src/pages/shared/Header/Header.js
@@ -1,6 +1,5 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import {Link, NavLink} from 'react-router-dom';
-import {themeChange} from 'theme-change';
 import {useEffect} from 'react';
 import toast from 'react-hot-toast';
 import './Header.css';
@@ -8,10 +7,34 @@ import {AuthContext} from '../../../contexts/ContextProvider/ContextProvider';
 const Header = () => {
 	// *get Context data
 	const {user, userLogOut} = useContext(AuthContext);
+	const [checked, setChecked] = useState('');
+	console.log('🚀🚀: Header -> checked', checked);
+
 	//*change theme function
 	useEffect(() => {
-		themeChange(false);
+		const data = localStorage.getItem('Theme');
+		let html = document.getElementsByTagName('html');
+		html[0].setAttribute('data-theme', data);
+		if (data === 'dark') {
+			setChecked(false);
+		} else {
+			setChecked(true);
+		}
 	}, []);
+	const changeTheme = () => {
+		// localStorage.setItem('Theme', checked);
+		setChecked(!checked);
+		let html = document.getElementsByTagName('html');
+		const theme = html[0].getAttribute('data-theme');
+		if (theme === 'dark') {
+			html[0].setAttribute('data-theme', 'light');
+			localStorage.setItem('Theme', 'light');
+		} else {
+			html[0].setAttribute('data-theme', 'dark');
+			localStorage.setItem('Theme', 'dark');
+		}
+	};
+
 	//*handle Logout function
 	const handleLogOut = () => {
 		userLogOut()
@@ -119,13 +142,41 @@ const Header = () => {
 				</div>
 
 				<div className="navbar-end hidden lg:flex">
-					<select
+					{/* <select
 						className="select select-primary w-24 mx-4 max-w-xs text-xs hidden lg:block"
 						data-choose-theme
 					>
 						<option value="">Dark</option>
 						<option value="light">Light</option>
-					</select>
+					</select> */}
+					<button onClick={() => changeTheme()} className=" text-white mr-4">
+						{checked ? (
+							<svg
+								stroke="currentColor"
+								fill="currentColor"
+								strokeWidth="0"
+								viewBox="0 0 16 16"
+								height="19"
+								width="19"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<path d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278z"></path>
+								<path d="M10.794 3.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387a1.734 1.734 0 0 0-1.097 1.097l-.387 1.162a.217.217 0 0 1-.412 0l-.387-1.162A1.734 1.734 0 0 0 9.31 6.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387a1.734 1.734 0 0 0 1.097-1.097l.387-1.162zM13.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.156 1.156 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.156 1.156 0 0 0-.732-.732l-.774-.258a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732L13.863.1z"></path>
+							</svg>
+						) : (
+							<svg
+								stroke="currentColor"
+								fill="currentColor"
+								strokeWidth="0"
+								viewBox="0 0 16 16"
+								height="19"
+								width="19"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<path d="M8 12a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z"></path>
+							</svg>
+						)}
+					</button>
 				</div>
 				{user?.uid ? (
 					<div className="dropdown dropdown-end ">
